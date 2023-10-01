9f25a89130e71fd4c3d41e03e6133f21d654442c
Marcel Müller <marcel-mueller@gmx.de>
2023-10-01 13:39:12 +0200

    Use variablesUse variables

--- Package.swift
+++ Package.swift
@@ -1,6 +1,9 @@
 // swift-tools-version:5.9
 import PackageDescription
 
+let webrtcVersion = "116.5845.0"
+let webrtcChecksum = "93290ee4dda999500d4b649487ba561479038b47079dfc5b368c4552eadd74aa"
+
 let package = Package(
     name: "WebRTC client library",
     platforms: [
@@ -14,9 +17,9 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC client library",
-            url: "https://github.com/nextcloud-releases/talk-clients-webrtc/releases/download/116.5845.0/WebRTC.xcframework.zip",
+            url: "https://github.com/nextcloud-releases/talk-clients-webrtc/releases/download/\(webrtcVersion)/WebRTC.xcframework.zip",
             // Generate checksum with `swift package compute-checksum WebRTC.xcframework.zip`
-            checksum: "93290ee4dda999500d4b649487ba561479038b47079dfc5b368c4552eadd74aa"
+            checksum: webrtcChecksum
         ),
     ]
 )
